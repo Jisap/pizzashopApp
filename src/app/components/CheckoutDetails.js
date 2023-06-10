f9b3d97d0a030edbc4f5b5143f5bb41d4b0d1f40
Jorge Matos <jisapjisap@gmail.com>
2023-06-10 16:07:03 +0200

    checkoutdetails

--- src/app/components/CheckoutDetails.js
+++ src/app/components/CheckoutDetails.js
@@ -6,16 +6,20 @@ const CheckoutDetails = ({ setModal }) => {
 
   const { cart, setCart, cartTotal } = useContext(CartContext)
 
+  console.log(cart)
+
   return (
     <div>
-      <div>
+      <div className="lg:gap-x-8 h-full lg:px-12 lg:py-8">
         {/* title */}
-        <h2>Shipping & Checkout</h2>
+        <h2 className="mb-6 text-[20px] uppercase font-extrabold text-center lg:text-left pt-6 lg:pt-0">
+          Shipping & Checkout
+        </h2>
 
         <div className="h-[86vh] lg:h-[47.5vh] flex flex-col lg:flex-row lg:gap-x-4">
           
           {/* box 1*/}
-          <div className="bg-green-400 flex-1 h-full overflow-y-auto lg:overflow-visible py-4 px-8 lg:py-0 lg:px-0">
+          <div className="flex-1 h-full overflow-y-auto lg:overflow-visible py-4 px-8 lg:py-0 lg:px-0">
             {/* input wrapper */}
             <div className="flex flex-col gap-4 h-full">
               {/* firstname && lastname */}
@@ -37,14 +41,45 @@ const CheckoutDetails = ({ setModal }) => {
               <div className="flex justify-between gap-x-4">
                 <input type="text" className="w-full input" placeholder="Block" />
                 <input type="text" className="w-full input" placeholder="Floor" />
-                <input type="text" className="w-full input" placeholder="Apartment" />
+                <input type="text" className="w-full input" placeholder="Apt. No." />
               </div>
+
+                {/* textarea */}
+                <div className="flex-1 h-full">
+                  <textarea className="textarea w-full h-full" placeholder="mentions (opcional)">
+
+                  </textarea>
+                </div>
             </div>
           </div>
 
           {/* box 2 */}
-          <div className="bg-yellow-400 flex-1 h-full lg:max-w-[40%] flex flex-col justify-between pt-3 px-8 lg:p-0">
-            box 2
+          <div className="flex-1 h-full lg:max-w-[40%] flex flex-col justify-between pt-3 px-8 lg:p-0">
+            <div className="border rounded-lg flex flex-col mb-4 p-4 h-full">
+       
+              <h3 className="text-base font-extrabold uppercase mb-4 border-b pb-4">Your order</h3>
+              
+              {/* items */}
+              <div className="overflow-y-scroll overflow-hidden scrollbar-thin scroll-thumb-gray-200 scrollbar-track-white-500 font-semibold
+              flex flex-col gap-y-4 h-[240px] py-2">
+                {cart.map((pizza, index) => {
+                  return (
+                    <div className="flex justify-between text-[15px]" key={index}>
+                      <div className="flex gap-x-2">
+                        <div className="capitalize">{ pizza.name }</div>
+                        <div>{ pizza.amount > 1 && `x ${pizza.amount}`}</div>
+                      </div>
+                      <div>${ parseFloat(pizza.price * pizza.amount).toFixed(2) }</div>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+
+            {/* place order btn */}
+            <button className="btn btn-lg gradient">
+                Place order
+            </button>
           </div>
 
         </div>
